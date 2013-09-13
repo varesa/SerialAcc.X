c85ca9b2a44bbf46767fcc20c23a828caf4863a5
Esa Varemo <esa@kuivanto.fi>
2013-09-13 19:16:04 +0300

    Added a bunch of files

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: esa
+ *
+ * Created on September 12, 2013, 3:45 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=SerialAcc.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/SerialAcc.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=serialacc.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/serialacc.x.tar
