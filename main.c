dcbec4ae1971ca9197e1d2d23fa26d226c0d1804
Esa Varemo <esa@kuivanto.fi>
2013-09-14 11:26:23 +0300

    Added some writing to the usb

--- main.c
+++ main.c
@@ -13,17 +13,46 @@
 
 #include "Microchip/USB/usb_common.h"
 #include "Microchip/USB/usb_device.h"
+#include "Microchip/USB/usb_function_cdc.h"
 
 #include "configwords.h"
+#include "usb_config.h"
 
 
 
 int main(int argc, char** argv) {
     USBInitialize();
 
+    int i = 1;
+
     while(TRUE) {
 
         USBDeviceTasks();
+
+
+        char buffer[64];
+        getsUSBUSART(buffer, 64);
+
+        if(USBUSARTIsTxTrfReady()) {
+            switch(i) {
+                case 0:
+                    putrsUSBUSART("Ab\r\n");
+                    i++;
+                    break;
+                case 1:
+                    putrsUSBUSART("cdE\r\n");
+                    i = 0;
+                    break;
+                default:
+                    i--;
+                    break;
+            }
+
+
+            //CDCTxService();
+        }
+
+        CDCTxService();
     }
 
     return (EXIT_SUCCESS);
